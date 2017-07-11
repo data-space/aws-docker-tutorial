e2754bec439aca922f54edc8591fd65b13e84a7f
David Oury <david@scatter.com>
2017-07-11 09:28:27 -0400

    removed user_data comment

--- main.tf
+++ main.tf
@@ -81,19 +81,6 @@ resource "aws_instance" "test" {
       private_key = "${file("${var.aws_key_file}")}"
     }
   }
-#  user_data  = <<EOF
-#              #!/bin/bash
-#							echo sdf > /tmp/sdf
-#							exec > /var/log/user-data.log 
-#      				sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-#      				sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-#      				sudo yum install docker-ce -y
-#      				sudo systemctl start docker
-#      				sudo yum install -y epel-release
-#      				sudo yum install -y python-pip
-#      				sudo pip install --upgrade pip
-#      				sudo pip install docker-compose
-#              EOF
 }
 
 ### OUTPUT
