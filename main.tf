bbfb49909f9aa22487d336cf202b8f37422728dd
David Oury <david@scatter.com>
2017-07-18 18:02:00 -0400

    updates

--- main.tf
+++ main.tf
@@ -19,54 +19,54 @@ variable "aws_key_file" {
 }
 
 variable "aws_instance_type" {
-	description = "AWS instance type"
-	default			= "t2.large"
+  description = "AWS instance type"
+  default      = "t2.large"
 }
 
 variable "aws_ami" {
-	description = "AWS AMI"
-	default			= "ami-9cbf9bf9"
+  description = "AWS AMI"
+  default      = "ami-9cbf9bf9"
 }
 
 variable "aws_root_volume_type" {
-	description = "Root device type"
-	default			= "gp2"
+  description = "Root device type"
+  default      = "gp2"
 }
 
 variable "aws_root_volume_size" {
-	description = "Root device size (GB)"
-	default			= "50"
+  description = "Root device size (GB)"
+  default      = "50"
 }
 variable "aws_root_volume_dot" {  
-	description = "Root device EBS volume: delete on terminate"
-	default			= "true"
+  description = "Root device EBS volume: delete on terminate"
+  default      = "true"
 }
 
 ### CODE 
 ###
 
 provider "aws" {
-	access_key = "${var.aws_access_key}"
+  access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region = "${var.aws_region}"
 }
 
 resource "aws_security_group" "datalab_test" {
   name = "datalab_test"
-	#vpc_id = "${aws_vpc.datalake_vpc.id}"
+  #vpc_id = "${aws_vpc.datalake_vpc.id}"
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["141.133.0.0/16"]
-		############# add your home IP address 
+    ############# add your home IP address 
   }
   ingress { 
     from_port   = 8888
     to_port     = 8888
     protocol    = "tcp"
     cidr_blocks = ["141.133.0.0/16"]
-		############# add your home IP address 
+    ############# add your home IP address 
   }
   egress {
     from_port       = 0
@@ -79,16 +79,16 @@ resource "aws_security_group" "datalab_test" {
 resource "aws_instance" "test" {
   ami                    = "${var.aws_ami}"
   instance_type          = "${var.aws_instance_type}"
-	key_name               = "${var.aws_key_name}"
+  key_name               = "${var.aws_key_name}"
   vpc_security_group_ids = ["${aws_security_group.datalab_test.id}"]
-	tags {
-  	Name = "datalab-test"
+  tags {
+    Name = "datalab-test"
   }
-	root_block_device {
-		delete_on_termination = "${var.aws_root_volume_dot}"
-		volume_size = "${var.aws_root_volume_size}"
-		volume_type = "${var.aws_root_volume_type}"
-	}
+  root_block_device {
+    delete_on_termination = "${var.aws_root_volume_dot}"
+    volume_size = "${var.aws_root_volume_size}"
+    volume_type = "${var.aws_root_volume_type}"
+  }
   # Install Docker and docker-compose
   provisioner "remote-exec" {
     inline = [
@@ -101,8 +101,8 @@ resource "aws_instance" "test" {
       "sudo yum install -y python-pip",
       "sudo pip install --upgrade pip",
       "sudo pip install docker-compose", 
-			"sudo yum install git -y"
-			]	
+      "sudo yum install git -y"
+      ]  
     connection {
       type        = "ssh"
       user        = "centos"
@@ -115,7 +115,7 @@ resource "aws_instance" "test" {
 ###
 
 output "aws_key_file" {
-	value = "${var.aws_key_file}"
+  value = "${var.aws_key_file}"
 }
 
 output "public_ip" {
